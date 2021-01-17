776db60636bd27851c46c61c2ddfb1f437e26dd6
Bhavleen <singhbhavleen3@gmail.com>
2021-01-17 19:08:49 +0530

    A star symbol in java

---
+++ A.java
@@ -0,0 +1,23 @@
+class A
+{
+	public static void main(String arr[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				if(j==1&&i==1)
+				{
+					System.out.print(" *");
+				}
+				else if((j==1||j==5)||((i==3||i==1))) 
+				{
+					System.out.print('*');
+				}
+				else
+					System.out.print(' ');
+			}
+			System.out.println();
+		}
+	}
+}

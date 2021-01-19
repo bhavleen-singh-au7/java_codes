143917be83b5a5568b998d31d7fb11964e6995a3
Bhavleen <singhbhavleen3@gmail.com>
2021-01-19 21:45:03 +0530

    Alpha

---
+++ Alpha.java
@@ -0,0 +1,14 @@
+class Alpha
+{
+	public static void main(String arr[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

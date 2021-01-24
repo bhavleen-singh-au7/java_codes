1b2bacd595caf3048de9393dda0af247f7807ea3
Bhavleen <singhbhavleen3@gmail.com>
2021-01-24 21:04:58 +0530

    Java Test Doc

---
+++ TestDoc.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+public class TextDoc extends Frame implements ActionListener
+{
+	Label l1,l2;
+	TextArea area;
+	Button bt;
+	TextDoc()
+	{
+		l1=new Label();
+		l1.setBounds(50,50,100,30);
+		
+		l2=new Label();
+		l2.setBounds(160,50,100,30);
+		
+		area=new TextArea();
+		area.setBounds(20,100,500,500);
+		
+		bt=new Button("Count Words");
+		bt.setBounds(100,400,100,30);
+		
+		add(l1); add(l2); add(area); add(bt);
+		
+		setSize(700,700);
+		setLayout(null);
+		setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		String text=area.getText();
+		String words[]=text.split("\\s"); // \\s = = reading the spaces
+		l1.setText("words: "+words.length);
+		l2.setText("Character: "+text.length());
+	}
+	public static void main(String[] arr)
+	{
+		new TextDoc();
+	}
+}

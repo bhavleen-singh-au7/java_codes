2b1cf1bc360bd9c04a530b324a1d281db93a8137
Bhavleen <singhbhavleen3@gmail.com>
2021-01-31 00:04:22 +0530

    Triangle In Java

---
+++ Triangle.java
@@ -0,0 +1,22 @@
+class Triangle
+{
+public static void main(String arr[])
+{
+ int a=0;
+ while(a<5)
+ {
+	 System.out.print("*");
+	 a++;
+ }
+ int b=0;
+ do
+ {
+	 System.out.println();
+ }
+ while(a<5);
+ {
+	 System.out.print("+");
+	 b++;
+ } 
+}
+}

10a5d35786fd7170169fe72e5a26bea0caf67dfb
Bhavleen <singhbhavleen3@gmail.com>
2021-02-01 07:52:21 +0530

    Java Program

---
+++ Simple.java
@@ -0,0 +1,19 @@
+class Simple
+{
+public static void main(String arr[])
+{
+	int a=0;
+do
+{
+	int b=0;
+do
+ {
+	 System.out.print("*");
+	 b++;
+ }while(b<=a);
+ System.out.println();
+ a++;
+}while(a<=5);
+
+}
+}

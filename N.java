e84a51a0d0af59c1ea62287b6be2bb44f5c51150
Bhavleen <singhbhavleen3@gmail.com>
2021-02-02 12:39:54 +0530

    N in JAVA

---
+++ N.java
@@ -0,0 +1,23 @@
+class N
+{
+	public static void main(String arr[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				if(j==1||j==5)
+				{
+					System.out.print("*");
+				}
+				else if(i==j)
+				{
+					System.out.print("*");
+				}
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}

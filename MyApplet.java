646c648c99a56afaa634f70f97fbf5a6429f3d44
Bhavleen <singhbhavleen3@gmail.com>
2021-02-06 11:19:40 +0530

    Learned Applet in java

---
+++ MyApplet.java
@@ -0,0 +1,12 @@
+import java.applet.Applet;
+import java.awt.*;
+//<applet code="MyApplet" width="400" height="400">  </applet>
+public class MyApplet extends Applet
+{
+	public void paint(Graphics e)
+	{
+		e.drawString("Lovely",100,200);
+	}
+	 
+} 
+

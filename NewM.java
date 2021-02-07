18b85bdf95bd35f61cd1554d88b36dfa42edf847
Bhavleen <singhbhavleen3@gmail.com>
2021-02-07 22:24:14 +0530

    new java code

---
+++ NewM.java
@@ -0,0 +1,12 @@
+class NewM
+{
+	public static void main(String arr[])
+	{
+		int s=0;
+		for(int i=0;i<arr.length;i++)
+		s=s+Integer.parseInt(arr[i]);
+	
+	
+	System.out.println("sum is "+s); 
+	}
+}

f5488d415fbd4a373a9746816bfe7851c57135e7
Bhavleen <singhbhavleen3@gmail.com>
2021-02-19 21:21:44 +0530

    Star A pattern in java

---
+++ StarA.java
@@ -0,0 +1,18 @@
+class StarA
+{
+	public static void main(String[] arr)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=2;j<=i;j++)
+			{
+				System.out.print(' ');
+			}
+			for(int k=1;k<=1;k++)
+			{
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}

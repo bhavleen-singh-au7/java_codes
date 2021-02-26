805a39b93cb0336de3361e0888d949a53b1f403b
Bhavleen <singhbhavleen3@gmail.com>
2021-02-26 21:49:31 +0530

    D in java

---
+++ D.java
@@ -0,0 +1,23 @@
+class D
+{
+	public static void main(String arr[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				if((i==1||i==5)||(j==2)) 
+				{
+					System.out.print('*');
+				}
+				else if(j==5)
+				{
+					System.out.print(" *");
+				}
+				else
+					System.out.print(' ');
+			}
+			System.out.println();
+		}
+	}
+}

3dd9255a920321ed5a98001c11a88f3a417e731e
Bhavleen <singhbhavleen3@gmail.com>
2021-03-01 22:58:25 +0530

    New keyword in Java

---
+++ New.java
@@ -0,0 +1,23 @@
+class New
+{
+	public static void main(String arr[])
+	{
+		int a=1;
+		
+		
+		for(int i=0;i<=10;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(a+" ");
+			a++;
+			}
+			System.out.println();
+		}
+	}
+}
+/**
+i=1
+j=1
+1 a=2
+**/

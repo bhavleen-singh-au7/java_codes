b6daf7eda9704fbeeb9e1e31cca62dc4b1975c61
Bhavleen <singhbhavleen3@gmail.com>
2021-03-07 20:25:20 +0530

    Java pro code

---
+++ Pro.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Pro
+{
+	public static void main(String arr[])
+	{
+		int i=0;
+		while(i<=5)
+		{
+			int j=0;
+			while(j<+i)
+			{
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}

a7502ff74aae34c803216b5ab1a77e3a9fbd1317
Bhavleen <singhbhavleen3@gmail.com>
2021-03-10 20:25:20 +0530

    Coding Practice in Java

---
+++ Rectangle.java
@@ -0,0 +1,102 @@
+class Rectangle
+{
+	public static void main(String arr[])
+	{/**
+		for(int q=1;q<=7;q++)
+		{
+			if(q==4||q==7)
+			{
+				for(int r=1;r<=10;r++)
+				{
+					System.out.print("*");
+				}
+			}
+			else
+			{
+				for(int s=1;s<=10;s++)
+				{
+					if(s==1||s==10)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}
+		for(int a=1;a<=3;a++)
+		{
+			for(int b=1;b<=8;b++)
+				{
+					if(b==1||b==2)
+					System.out.print(" ");
+					else 
+					{
+						if(a==1)
+						{
+						for(int q=1;q<=6;q++)
+						{
+							System.out.print("*");
+						}
+						System.out.println();
+						}
+						else{
+						System.out.print("*");
+						for(int r=1;r<=4;r++)
+						System.out.print(" ");
+						System.out.print("*");
+						System.out.println();
+						}
+					}
+					
+				
+				}
+			}
+			
+			else
+			{
+				for(int c=1;c<=10;c++)
+				{
+					if(c==1||c==10)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}**/
+		
+		for(int i=1;i<=4;i++)
+		{
+			if(i==1||i==4)
+			{
+				for(int j=1;j<=10;j++)
+				{
+				System.out.print("*");
+				}
+			}
+			else
+			{
+				for(int k=1;k<=10;k++)
+				{
+					if(k==1||k==10)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+}

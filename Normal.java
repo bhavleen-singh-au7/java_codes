99b3ebb451bfe6c9d392297d7b99d6d2097a4a02
Bhavleen <singhbhavleen3@gmail.com>
2021-03-11 20:25:20 +0530

    Coding Practice in Java

---
+++ Normal.java
@@ -0,0 +1,13 @@
+class Normal {
+int nMeth(int x,int y)
+{
+int z=x+y;
+System.out.println(z);
+return 0;
+}
+public static void main(String arr[])
+{
+Normal obj=new Normal();
+obj.nMeth(10,13);
+}
+}

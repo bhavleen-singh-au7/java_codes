b3a7ef29bd3110e73820608fff73479479022808
Bhavleen <singhbhavleen3@gmail.com>
2021-04-10 12:00:58 +0530

    Learning in Java

---
+++ Learn1.java
@@ -0,0 +1,16 @@
+//concat() //lengh()
+class Learn1
+{
+	public static void main(String arr[])
+	{
+		String s1=("This is my Computer");
+		String s2=("Meri Machine");
+		String s3=s1.concat(s2);
+		//System.out.println("s1 + s2 ="+s3); line 9 and 10 works the same
+		System.out.println(s1+s2);
+		int si=s1.length();
+		int sii=s2.length();
+		System.out.println(si);
+		System.out.println(sii);
+	}
+}
